64382c9232202a857a4d2cb3f9d3d99b54a63612
Abhishek9607 <abhi7549ku@gmail.com>
2023-10-29 10:37:08 +0530

    Added HomeCarousel, HomeMoviesCArd

---
+++ Movies-Booking-App/src/Components/HomeMoviesCard/HomeMoviesCard.tsx
@@ -0,0 +1,22 @@
+type HomeMoviesCardProps = {
+    movieImage: string
+}
+import { AiFillStar } from 'react-icons/Ai';
+
+function HomeMoviesCard({movieImage} : HomeMoviesCardProps) {
+  return (
+    <div className="flex flex-col items-center justify-center h-[350px] w-[222px] p-5">
+        <img 
+           src={movieImage} 
+           className="h-[95%] w-full rounded-tr-lg rounded-tl-lg"
+        />
+        <div className="bg-black w-full text-white text-center rounded-br-lg rounded-bl-lg ">
+        <AiFillStar className='inline text-lg text-red-500  '/>
+           <span>9/10</span>  {}
+           <span>52.5k Votes</span>
+        </div>
+    </div>
+  )
+}
+
+export default HomeMoviesCard
--- Movies-Booking-App/src/Pages/Home.tsx
+++ Movies-Booking-App/src/Pages/Home.tsx
@@ -1,13 +1,19 @@
+import MoviePoster from '../Assets/tejas.png'
 import HomeCarousel from "../Components/HomeCarousel/HomeCarousel";
-import Navbar from "../Components/Navbar";
-
+import HomeMoviesCard from "../Components/HomeMoviesCard/HomeMoviesCard";
+import Navbar from "../Components/Navbar" 
 
 function Home() {
   return (
     <>
      <Navbar />
      <HomeCarousel />
-    <div>Home</div>
+   <div className='flex items-center gap-6 justify-center'>
+   <HomeMoviesCard  movieImage={MoviePoster} />
+   <HomeMoviesCard  movieImage={MoviePoster} />
+   <HomeMoviesCard  movieImage={MoviePoster} />
+   <HomeMoviesCard  movieImage={MoviePoster} />
+   </div>
     </>
   )
 }
